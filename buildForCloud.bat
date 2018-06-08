3f6b65fe9bb7cdac245b2a475772f324d327bbc6
crypto-coder <chris.williams@puremoneysystems.com>
2018-06-08 09:01:03 -0400

    Switching CI/CD to windows build environment

---
+++ buildForCloud.bat
@@ -0,0 +1,18 @@
+md prod\public
+xcopy frontend\dist prod\public /E /Y
+
+md prod\bin
+xcopy backend\bin prod\bin /E /Y
+
+md prod\dist
+xcopy backend\dist prod\dist /E /Y
+
+md prod\shared
+xcopy backend\shared prod\shared /E /Y
+
+md prod\node_modules
+xcopy backend\node_modules prod\node_modules /E /Y
+
+copy backend\package.json prod\
+
+copy backend\server.js prod\
